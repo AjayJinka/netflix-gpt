600aaee8cf8888f1fc1c584359bad3d19bf2cd4d
AjayJinka <ajay.jinka@gmail.com>
2024-05-15 22:57:15 +0530

    css update

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,8 +4,10 @@ const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video pt-36 px-12 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-1/2">{overview}</p>
-      <div className="flex gap-4">
+      <div className="w-1/2 line-clamp-3 hover:line-clamp-none hover:w-full cursor-pointer">
+        <p className="py-6 text-lg">{overview}</p>
+      </div>
+      <div className="flex gap-4 mt-4">
         <button className="bg-white text-black px-8 py-2 text-xl text-center bg-opacity-100 rounded hover:bg-opacity-80 font-bold">
           ▶️ Play
         </button>
